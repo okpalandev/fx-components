a3f5717a340203f380c86482437a5a2c3579e96a
Nnamdi <koodu-platform@protonmail.com>
2024-05-30 15:51:16 +0100

    added-cored

--- src/utils/fx-parallax/fx-parallax.ts
+++ src/utils/fx-parallax/fx-parallax.ts
@@ -1,5 +1,5 @@
 import { Fx } from '../helpers';
-export {};
+export { };
 
 declare global {
     interface HTMLElementTagNameMap {
@@ -8,32 +8,74 @@ declare global {
 }
 
 namespace FxParallax {
-   export  interface FxParallax {
+    export interface FxParallax {
         constructor(): void;
         prototype: {
             handleScroll: () => void;
             isScrolling: boolean;
         } | undefined;
-    }    
-   
-    export declare class FxParallax {
+    }
+
+    export declare class FxParallax  extends HTMLElement {
         constructor();
         handleScroll(): void;
         isScrolling: boolean;
     }
 
-    export function ParallaxMixin<T extends new (...args: any[]) => {}>(Base: T) {
-        return class extends Base {
-            handleScroll: any;
+    export function FxParallaxMixin<T extends new (...args: any[]) => {}>(Base: T) {
+        return class extends HTMLElement {
             isScrolling: boolean;
             constructor(...args: any[]) {
-                super(...args);
+                super(...args as []);
                 this.handleScroll = this.handleScroll.bind(this);
                 this.isScrolling = false;
+                this.shadowRoot?.addEventListener('scroll', this.handleScroll);
+                this.attachShadow({ mode: 'open' });
+
             }
+            static get observedAttributes() {
+                return ['fx-data-speed', 'fx-data-direction', 'fx-data-offset'];
+            }
+            connectedCallback() {
+                this.handleScroll();
+                window.addEventListener('scroll', this.handleScroll);
+            }
+            disconnectedCallback() {
+                window.removeEventListener('scroll', this.handleScroll);
+            }
+            attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+                if (oldValue !== newValue) {
+                    this.handleScroll();
+                }
+            }
+
+            handleScroll() {
+                if (!this.isScrolling) {
+                    window.requestAnimationFrame(() => {
+                        this.style.transform = `translateY(${window.scrollY * Number(this.speed)}px)`;
+                        this.isScrolling = false;
+                    });
+                }
+                this.isScrolling = true;
+            }
+
+            get speed() {
+                const self = this;
+                return Number(self.getAttribute('fx-data-speed')) || 0.5;
+            }
+            get direction() {
+                return this.getAttribute('fx-data-direction') || 'vertical';
+            }
+            get offset() {
+                return this.getAttribute('fx-data-offset') || 0;
+            }
+
         }
+
     }
 
+    export const FxParallaxElement = Fx._compose(HTMLElement, FxParallaxMixin);
+    customElements.define('fx-parallax', FxParallaxElement);
 }
 
-export { FxParallax as default };
+export default FxParallax.FxParallax;
