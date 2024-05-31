f04d71d242dd534802e0d3583e5a3e89131bb189
Nnamdi <koodu-platform@protonmail.com>
2024-05-31 18:21:54 +0100

    a

---
+++ dist/effects/index.d.ts
@@ -0,0 +1,4 @@
+export * from './fx-components';
+export * from './/fx-parallax/fx-parallax';
+export * from './fx-tooltip/fx-tooltip';
+export * from './fx-observer/fx-observer';
---
+++ dist/fx-card/fx-card.d.ts
@@ -0,0 +1,11 @@
+declare const FxCard_base: any;
+declare class FxCard extends FxCard_base {
+    static styles: string;
+    constructor();
+    connectedCallback(): void;
+    render(): void;
+    attributeChangedCallback(name: string, oldValue: any, newValue: any): void;
+    observeAttributes(): void;
+    setInitialContent(): void;
+}
+export { FxCard };
