f8f8d10ffd0ab93721e366bce45f036b90f344ff
Nnamdi <koodu-platform@protonmail.com>
2024-06-01 08:39:47 +0100

    a

--- src/fx-components.ts
+++ src/fx-components.ts
@@ -1,5 +1,9 @@
 // utitlies 
 export * from './utils/';
+
+// effects
+export * from './fx-effects/fx-effects';
+
 // components
 export * from './fx-card/fx-card';
 export * from './fx-tooltip/fx-tooltip';
