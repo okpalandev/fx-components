dcb31d61a019f080ff3a3323a2675a2073de8cc6
Nnamdi <koodu-platform@protonmail.com>
2024-06-01 08:38:14 +0100

    a

--- src/fx-components.ts
+++ src/fx-components.ts
@@ -1,3 +1,6 @@
+// utitlies 
+export * from './utils/';
+// components
 export * from './fx-card/fx-card';
 export * from './fx-tooltip/fx-tooltip';
-export * from './fx-tooltip/fx-tooltip';
+export * from './fx-modal/fx-modal';
