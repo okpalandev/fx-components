31acb5cc4b4f327f8b38c54f302a731069f96607
Nnamdi <koodu-platform@protonmail.com>
2024-06-02 12:01:34 +0100

    a

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,10 @@
-export default {
-    preset: '@open-wc/testing',
-    testEnvironment: 'jsdom',
+module.exports = {
     transform: {
       '^.+\\.ts$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'js', 'json'],
     testMatch: ['**/*.spec.ts'],
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testEnvironment: 'jsdom',
   };
   
