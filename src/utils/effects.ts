4e1df0bad3a8ab0e46c1c797a2c486b71b74f9e7
Nnamdi <koodu-platform@protonmail.com>
2024-06-02 15:13:31 +0100

    a

--- src/utils/effects.ts
+++ src/utils/effects.ts
@@ -1,66 +1,49 @@
-export {};
-export default Effects;
+export { };
+
 /**
- * Namespace containing utility functions for Effects components.
+ * Composes a class with one or more mixins.
+ * @param ctor - The base class to compose with mixins.
+ * @param mixins - The mixins to apply to the base class.
+ * @returns The composed class.
  */
-export namespace Effects {
-    /**
-     * Composes a class with one or more mixins.
-     * @param ctor - The base class to compose with mixins.
-     * @param mixins - The mixins to apply to the base class.
-     * @returns The composed class.
-     */
-    export function _compose(ctor = HTMLElement, ...mixins: any[]) {
-        return mixins.reduce((BaseClass, mixin) => mixin(BaseClass), ctor);
-    }; 
+export function _compose(ctor = HTMLElement, ...mixins: any[]) {
+    return mixins.reduce((BaseClass, mixin) => mixin(BaseClass), ctor);
+};
 
-    /**
-     * Interpolates a template string with CSS styles.
-     * @param strings - The template strings.
-     * @param values - The values to interpolate into the template.
-     * @returns The interpolated CSS string.
-     */
-    export function _css(strings: TemplateStringsArray, ...values: any[]) {
-        return strings.reduce((acc, str, i) => {
-            return acc + str + (values[i] || '');
-        }, '');
-    };
+/**
+ * Interpolates a template string with CSS styles.
+ * @param strings - The template strings.
+ * @param values - The values to interpolate into the template.
+ * @returns The interpolated CSS string.
+ */
+export function _css(strings: TemplateStringsArray, ...values: any[]) {
+    return strings.reduce((acc, str, i) => {
+        return acc + str + (values[i] || '');
+    }, '');
+};
 
-    /**
-     * Interpolates a template string with HTML content and returns a document fragment.
-     * @param strings - The template strings.
-     * @param values - The values to interpolate into the template.
-     * @returns The document fragment containing the interpolated HTML.
-     */
-    export function _html(strings: TemplateStringsArray, ...values: any[]) {
-        return new Function('return `' + strings.reduce((acc, str, i) => {
-            return acc + str + (values[i] || '');
-        }, '') + '`')();
-    };
+/**
+ * Interpolates a template string with HTML content and returns a document fragment.
+ * @param strings - The template strings.
+ * @param values - The values to interpolate into the template.
+ * @returns The document fragment containing the interpolated HTML.
+ */
+export function _html(strings: TemplateStringsArray, ...values: any[]) {
+    return new Function('return `' + strings.reduce((acc, str, i) => {
+        return acc + str + (values[i] || '');
+    }, '') + '`')();
+};
 
-    /**
-     * Creates a <style> element with the specified styles.
-     * @param styles - The CSS styles.
-     * @returns The created <style> element.
-     */
-    export function _style(styles: string) {
-        const style = document.createElement('style');
-        style.textContent = styles;
-        return style;
-    };
-    
-    /**
-     * Creates a <template> element with the specified HTML content.
-     * @param html - The HTML content.
-     * @returns The created <template> element.
-     */
-    export function _template(html: string) {
-        const template = document.createElement('template');
-        template.innerHTML = html;
-        return template;
-    };
 
-    
 
-    
-}
+/**
+ * Creates a <template> element with the specified HTML content.
+ * @param html - The HTML content.
+ * @returns The created <template> element.
+ */
+export function _template(html: string) {
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template;
+};
+
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 
-export *   from './effects';
+export *  as Effect  from './effects';
 
